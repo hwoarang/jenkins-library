12affec0ec9f131eb4c8949b374ecef544613c10
Markos Chandras <mchandras@suse.de>
2019-02-13 10:33:43 +0000

    vars: getRepoInfo: Add new function to get repository information
    Right now, the code assumes that everything is hosted on github.com in
    the kubic-project organization. This may not be true in the future so
    lets have a definitive list containing all the repository information.
    
    (cherry picked from commit 667d0c1afd8fe9c9e637d3d44f93f9aae285b933)

---
+++ vars/getRepoInfo.groovy
@@ -0,0 +1,25 @@
+// Copyright 2019 SUSE LINUX GmbH, Nuernberg, Germany.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+def call(String repository) {
+    // Definitive list of repositories
+    def Repositories = [
+        "automation": ["github.com", "kubic-project"],
+        "jenkins-library": ["github.com", "kubic-project"],
+        "salt": ["github.com", "kubic-project"],
+        "velum": ["github.com", "kubic-project"],
+        "caasp-container-manifests": ["github.com", "kubic-project"],
+    ]
+    return ["hosting": Repositories[repository][0], "organization": Repositories[repository][1]]
+}
